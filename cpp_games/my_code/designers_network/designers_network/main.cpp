4488f42107cdf31c30bc152a3b730d272da2c8b7
KotelnikovRoman <kotelnikovrm@gmail.com>
2020-01-22 12:51:14 +0800

    update cpp

--- cpp_games/my_code/designers_network/designers_network/main.cpp
+++ cpp_games/my_code/designers_network/designers_network/main.cpp
@@ -7,4 +7,23 @@ int main() {
 	string password;
 	bool success;
 
+	setlocale(LC_ALL, "Russian");
+	//Game designer network
+	cout << "Сеть геймдизайнеров\n";
+	
+	do {
+		cout << "Имя пользователя: ";
+		cin >> username;
+		cout << "\nПароль: ";
+		cin >> password;
+
+		if (username == "Miler" && password == "civilization") {
+			cout << "Hello " << username;
+			success = true;
+		}
+		else if (username == "Vayler" && password == "minicraft") {
+			success = true;
+		}
+	} while (!success);
+	return 0;
 }
