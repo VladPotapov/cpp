789efab6b03e42a51287d00de544610619d5711a
KotelnikovRoman <kotelnikovrm@gmail.com>
2020-01-23 17:16:55 +0800

    TX_Library освоил методы txCreateWindow, txSetColor, txSetDefault

--- python_c++/path1/my_code/tx_lib1/tx_lib1/main.cpp
+++ python_c++/path1/my_code/tx_lib1/tx_lib1/main.cpp
@@ -1,5 +1,18 @@
 #include "c:\Server\data\htdocs\cpp\libs\TX\TXLib.h"
 
+void win(int x1, int y1, int x2, int y2, double sizeX, double sizeY, COLORREF color);
+
 int main() {
-	txCreateWindow(500, 500);
+	win(20, 50, 100, 0, 500, 500, TX_RED);
+	return 0;
+}
+
+void win(int x1, int y1, int x2, int y2, double sizeX, double sizeY, COLORREF color) {
+	txCreateWindow(sizeX, sizeY);
+	txSetColor(color);
+	txLine(x1, y1, x1 + x2, y1 + y2);
+	
+	//устанавливает значения по умолчанию
+	txSetDefaults();
+	txLine(x1, y1 * 2, x1 + x2, (y1 * 2) + y2);
 }
