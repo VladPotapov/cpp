e54c51397fbe0fe103d2d3fbefcc5f44f623b6e4
Roman <kotelnikovrm@gmail.com>
2020-05-20 19:32:02 +0800

    update counter in cpp_games

--- cpp_games/my_code/counter/counter/main.cpp
+++ cpp_games/my_code/counter/counter/main.cpp
@@ -1,6 +1,27 @@
 #include <iostream>
+#include <Windows.h>
 using namespace std;
 
 int main() {
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+	cout << "отсчет вперед:\n";
+	for (int i = 0; i < 10; ++i) {
+		cout << i << " ";
+	}
+	cout << "\n\nотсчёт назад:\n";
+	for (int i = 9; i >= 0; --i) {
+		cout << i << " ";
+	}
+	cout << "\n\nотсчёт по 5:\n";
+	for (int i = 0; i <= 50; i += 5) {
+		cout << i << " ";
+	}
+	cout << "\n\nподсчет с вложенными циклами for:\n";
+	int num = 0;
+	for (; num < 10;) {
+		cout << num << " ";
+		num++;
+	}
 	return 0;
 }
